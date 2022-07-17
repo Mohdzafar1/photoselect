38b65deae22b6c121606b7227d99336499f6a8c7
Mohd zafar <68924688+Mohdzafar1@users.noreply.github.com>
2022-07-17 22:39:02 +0530

    photo select-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export class App extends Component{
+
+   state={
+    profileImg:"https://cdn.onlinewebfonts.com/svg/img_364496.png"
+   } 
+    imageHandler=(e)=>{
+      const reader =new FileReader();
+      reader.onload=()=>{
+        if(reader.readyState === 2){
+          this.setState({profileImg:reader.result})
+        }
+      }
+      reader.readAsDataURL(e.target.files[0])
+    }
+    
+  render(){
+     const {profileImg}=this.state
+    return(
+      <>
+   <div className="page">
+    <div className="container">
+      <h1 className="heading">Add your Image</h1>
+      <div className="img-holder">
+        <img src={profileImg} id="img" className='img'/>
+      </div>
+      <input type="file"name="image-upload"id="input" accept='image/*' onChange={this.imageHandler}/>
+      <div className="label">
+        <label htmlFor="input" className='image-upload-label'> 
+        <i class="fas fa-image fa-1x"></i>
+        Choose your photo
+        </label>
+      </div>
     </div>
-  );
+   </div>
+      </>
+    )
+  }
 }
 
 export default App;
